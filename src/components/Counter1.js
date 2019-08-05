cba0fbc38552583cb46c32a59e2328ef1b9e5555
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-05 15:38:26 -0400

    useReducer - Simple state and action

---
+++ src/components/Counter1.js
@@ -0,0 +1,28 @@
+import React, {useReducer} from 'react';
+
+const initialState = 0;
+
+const reducerFunction = (currentState, action) => {
+  switch (action) {
+    case "+": return currentState + 1;
+    case "-": return currentState - 1;
+    case "reset": return initialState;
+    default: return currentState;
+  }
+}
+
+function Counter1(){
+  const [count, dispatch] = useReducer(reducerFunction, initialState);
+
+
+  return(
+    <div>
+      <div>Count: {count}</div>
+      <button onClick={() => dispatch("+")}>+</button>
+      <button onClick={() => dispatch("-")}>-</button>
+      <button onClick={() => dispatch("reset")}>0</button>
+    </div>
+  )
+}
+
+export default Counter1;
