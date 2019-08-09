c3f252e74ad8e42de51baddfbbe1bb4ba005e13c
mudrakpatel <mudrakpatel96@gmail.com>
2019-08-09 17:48:18 -0400

    Fetching data with useReducer: Part 1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 import React, {useReducer} from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
 //import Counter1 from './components/Counter1';
 //import Counter2 from './components/Counter2';
 //import Counter3 from './components/Counter3';
-import ComponentA from './components/ComponentA';
-import ComponentB from './components/ComponentB';
-import ComponentC from './components/ComponentC';
-
-export const CountContext = React.createContext();
-
-const initialState = 0;
-
-const reducerFunction = (currentState, action) => {
-  switch (action) {
-    case "+": return currentState + 1;
-    case "-": return currentState - 1;
-    case "reset": return initialState;
-    default: return currentState;
-  }
-}
+// import ComponentA from './components/ComponentA';
+// import ComponentB from './components/ComponentB';
+// import ComponentC from './components/ComponentC';
+//
+// export const CountContext = React.createContext();
+//
+// const initialState = 0;
+//
+// const reducerFunction = (currentState, action) => {
+//   switch (action) {
+//     case "+": return currentState + 1;
+//     case "-": return currentState - 1;
+//     case "reset": return initialState;
+//     default: return currentState;
+//   }
+// }
 
 function App() {
-  const [count, dispatch] = useReducer(reducerFunction, initialState);
+  //const [count, dispatch] = useReducer(reducerFunction, initialState);
 
   return (
-    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
-      <div className="App">
-        {/*<Counter3/>*/}
-        {/*<Counter2/>*/}
-        {/*<Counter1/>*/}
-        Count: {count}
-        <ComponentA/>
-        <ComponentB/>
-        <ComponentC/>
-      </div>
-    </CountContext.Provider>
+    <div className="App">
+      <DataFetchingOne/>
+    </div>
   );
 }
 
